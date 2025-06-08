3ccd9a6131242021eb1334e0f73aa11c8f33a874
miesho <marcin.ryzewskii@gmail.com>
2025-06-08 23:00:11 +0200

    Refactor main.zig...enable terminal's raw mod.

--- src/main.zig
+++ src/main.zig
@@ -2,55 +2,61 @@ const std = @import("std");
 const print = std.debug.print;
 const heap = std.heap.page_allocator;
 const line = std.ArrayList(u8).init(heap);
-const os = std.os;
-const termios = std.os.termios;
+
+const c = @cImport({
+    @cInclude("termios.h");
+    @cInclude("unistd.h");
+});
+
+
+const CLEAR_TERMINAL = "\x1b[2J\x1b[H";
 
 const Editor = struct {
-    line_pos: i32,
-    char_pos: i32,
+    x: i32,
+    y: i32,
     allocator: std.mem.Allocator,
-    line_buffer: std.ArrayList(std.ArrayList(u8)),
+    buffer: std.ArrayList(std.ArrayList(u8)),
 
     pub fn init(allocator : std.mem.Allocator) Editor {
         return Editor {
-            .line_pos    = 0,
-            .char_pos    = 0,
+            .x    = 0,
+            .y    = 0,
             .allocator   = allocator,
-            .line_buffer = std.ArrayList(std.ArrayList(u8)).init(allocator)
+            .buffer = std.ArrayList(std.ArrayList(u8)).init(allocator)
         };
     }
 
     pub fn putc(self: *Editor, ch: u8) !void {
-        try self.line_buffer
-            .items[@intCast(self.line_pos)].insert(@intCast(self.char_pos), ch);
+        try self.buffer
+            .items[@intCast(self.x)].insert(@intCast(self.y), ch);
     }
 
     pub fn new_line(self: *Editor) !void {
-        if (self.line_pos == self.line_buffer.items.len) {
-            try self.line_buffer.append(std.ArrayList(u8).init(self.allocator));
+        if (self.x == self.buffer.items.len) {
+            try self.buffer.append(std.ArrayList(u8).init(self.allocator));
         }
-        try self.line_buffer
-            .insert(@intCast(self.line_pos), std.ArrayList(u8).init(self.allocator));
+        try self.buffer
+            .insert(@intCast(self.x), std.ArrayList(u8).init(self.allocator));
     }
 
-    pub fn mv_cursor(self: *Editor, line_pos: i32, char_pos: i32) void {
-        self.line_pos += line_pos;
-        if (self.line_pos < 0) {
-            self.line_pos = 0;
-        } else if (self.line_pos > self.line_buffer.items.len) {
-            self.line_pos = @intCast(self.line_buffer.items.len);
+    pub fn mv_cursor(self: *Editor, x: i32, y: i32) void {
+        self.x += x;
+        if (self.x < 0) {
+            self.x = 0;
+        } else if (self.x > self.buffer.items.len) {
+            self.x = @intCast(self.buffer.items.len);
         }
 
-        self.char_pos += char_pos;
-        if (self.char_pos < 0) {
-            self.char_pos = 0;
-        } else if (self.char_pos > self.line_buffer.items[@intCast(self.line_pos)].items.len) {
-            self.char_pos = @intCast(self.line_buffer.items[@intCast(self.line_pos)].items.len);
+        self.y += y;
+        if (self.y < 0) {
+            self.y = 0;
+        } else if (self.y > self.buffer.items[@intCast(self.x)].items.len) {
+            self.y = @intCast(self.buffer.items[@intCast(self.x)].items.len);
         }
     }
 
     pub fn popc(self: *Editor) !void {
-        _ = self.line_buffer.items[@intCast(self.line_pos)].orderedRemove(@intCast(self.char_pos));
+        _ = self.buffer.items[@intCast(self.x)].orderedRemove(@intCast(self.y));
     }
 
     pub fn load() void {
@@ -63,46 +69,39 @@ const Editor = struct {
         });
         defer file.close();
 
-        for (self.line_buffer.items) |itm| {
+        for (self.buffer.items) |itm| {
             try file.writeAll(itm.items);
         }
     }
 
     pub fn debug_print(self: *Editor) void {
-        for (self.line_buffer.items) |item| {
+        for (self.buffer.items) |item| {
             print("{s}\n",.{item.items});
         }
     }
 
 };
 
-pub fn enableRawMode() !termios.Termios {
-    var original = try termios.tcgetattr(os.STDIN_FILENO);
-    var raw = original;
+pub fn enable_raw_mod() void {
+    var raw: c.struct_termios = undefined;
+    _ = c.tcgetattr(c.STDIN_FILENO, &raw);
 
-    raw.lflag &= ~termios.ICANON; // disable canonical mode
-    raw.lflag &= ~termios.ECHO;   // disable echo
-    // raw.lflag &= ~termios.ISIG;   // disable Ctrl-C and Ctrl-Z
-    raw.iflag &= ~(termios.IXON | termios.ICRNL); // disable Ctrl-S/Q and CR-to-NL
-    raw.c_oflag &= ~termios.OPOST; // disable output processing
-    raw.c_cc[termios.VMIN] = 1;
-    raw.c_cc[termios.VTIME] = 0;
+    raw.c_lflag &= ~(@as(c.tcflag_t, c.ECHO));
+    raw.c_lflag &= ~(@as(c.tcflag_t, c.ICANON));
 
-    try termios.tcsetattr(os.STDIN_FILENO, termios.TCSAFLUSH, &raw);
-
-    return original;
+    _ = c.tcsetattr(c.STDIN_FILENO, c.TCSAFLUSH, &raw);
 }
 
-pub fn disableRawMode(original: termios.Termios) void {
-    _ = termios.tcsetattr(os.STDIN_FILENO, termios.TCSAFLUSH, &original);
+fn disableRawMode(original: *const c.struct_termios) void {
+    _ = c.tcsetattr(0, c.TCSAFLUSH, original);
 }
-
 
 pub fn main() !void {
-    enableRawMode();
-    disableRawMode();
-    print("\x1b[2J", .{});
-    print("\x1b[H", .{});
+    enable_raw_mod();
+    var ch: c_char = undefined;
+    print("{s}", .{CLEAR_TERMINAL});
+
+    while ((c.read(c.STDIN_FILENO, &ch, 1) == 1 ) and (ch != 'q')) { }
 }
 
 

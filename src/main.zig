ba64498a490994d2991c4e28d471c25e3f15a129
miesho <marcin.ryzewskii@gmail.com>
2025-07-13 21:22:00 +0200

    Update tui demo

--- src/main.zig
+++ src/main.zig
@@ -13,29 +13,47 @@ const c = @cImport({
 
 const CLEAR_TERMINAL = "\x1b[2J\x1b[H";
 
-
 pub fn main() !void {
     tui.enable_raw_mod();
+    tui.cursor_off();
     tui.clear();
 
-    var ed = editor.Editor.init(heap);
-    try ed.new_line();
-    try ed.putc('q');
-    
-    tui.puts("===========", 15, 15);
-    tui.puts("|", 14, 16);
-    tui.puts("|", 26, 16);
-    tui.puts("===========", 15, 17);
+    // const buffer: []const u8 = "test\nsome\nbuffer\nhelllllo\n";
+    // col:181 row:50
 
-    tui.puts("XxxxDD", 13, 13);
+    var frame_buffer = try tui.FrameBuffer.init(heap, tui.canvas_size().ws_row - 1, tui.canvas_size().ws_col, 0, 0);
+    defer frame_buffer.deinit(heap);
 
+    var render_bench: [181*49]u8 = undefined;
+    // @memset(&render_bench, 'X');
+    frame_buffer.puts(&render_bench, 0, 0);
+    // frame_buffer.putc(' ', 20, 20);
 
-    tui.set_currsor(0,0);
-    tui.canvas_size();
-    
-    var quit: bool = false;
-    while (!quit) { 
-        quit = tui.evnet_loop();
+    // tui.render_frame_buffer(&frame_buffer);
+    var x_pos: u32 = 0;
+    var y_pos: u32 = 0;
+    frame_buffer.putc('X', x_pos, y_pos);
+
+    while(true) { 
+        var timer = try std.time.Timer.start();
+        frame_buffer.render();
+        const elapsed = timer.read();
+        
+        frame_buffer.putc(' ', x_pos, y_pos);
+
+        if (x_pos == 181) { 
+            y_pos += 1; x_pos = 0;
+        }
+        x_pos += 1;
+        if (x_pos == 181 and y_pos == 49) {
+            x_pos = 0; y_pos = 0;
+        }
+
+        frame_buffer.putc('X', x_pos, y_pos);
+
+        tui.set_currsor(0, 100);
+        print("ns: {}", .{elapsed});
+        // std.time.sleep(1_000_000);
     }
 }
 

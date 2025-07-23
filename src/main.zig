f299bce67e9810a24e71a166f07cb134de835664
miesho <marcin.ryzewskii@gmail.com>
2025-07-23 21:21:03 +0200

    src/main.zig: change: refactor

--- src/main.zig
+++ src/main.zig
@@ -13,50 +13,28 @@ const c = @cImport({
 
 const CLEAR_TERMINAL = "\x1b[2J\x1b[H";
 
+const app = editor.init(heap);
+
+pub fn ed() !bool {
+    var bench = try tui.Bench.start();
+    
+    const key = tui.get_input();
+
+    bench.stop();
+    bench.fps_print(0, 100);
+
+    if (key == 'q') { return false; }
+    return true;
+}
+
+
 pub fn main() !void {
     tui.enable_raw_mod();
     tui.cursor_off();
     defer tui.cursor_on();
     tui.clear();
 
-    // const buffer: []const u8 = "test\nsome\nbuffer\nhelllllo\n";
-    // col:181 row:50
-
-    var frame_buffer = try tui.FrameBuffer.init(heap, tui.canvas_size().ws_row - 1, tui.canvas_size().ws_col, 0, 0);
-    defer frame_buffer.deinit(heap);
-
-    var render_bench: [181*49]u16 = undefined;
-    @memset(&render_bench, ' ');
-    frame_buffer.puts(&render_bench, 0, 0);
-    // frame_buffer.putc(' ', 20, 20);
-
-    // tui.render_frame_buffer(&frame_buffer);
-    var x_pos: u32 = 0;
-    var y_pos: u32 = 0;
-    // frame_buffer.putc('█', x_pos, y_pos);
-
-    while(tui.get_input() != 'q') { 
-        var timer = try std.time.Timer.start();
-        frame_buffer.render();
-
-        frame_buffer.putc(' ', x_pos, y_pos);
-
-        if (x_pos == 181) { 
-            y_pos += 1; x_pos = 0;
-        }
-        x_pos += 1;
-        if (x_pos == 181 and y_pos == 48) {
-            x_pos = 0; y_pos = 0;
-        }
-
-        frame_buffer.putc('█', x_pos, y_pos);
-
-        const elapsed = timer.read();
-        const float_elapsed: f64 = @floatFromInt(elapsed);
-        tui.set_currsor(0, 100);
-        print("fps: {d:.2}", .{1_000_000_000 / float_elapsed});
-        // std.time.sleep(1_000_000);
-    }
+    while(try ed()) { }
 }
 
 

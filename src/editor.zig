5584f5ba3707eafe740f2d98e47f1d6c7fb4c7da
miesho <marcin.ryzewskii@gmail.com>
2025-07-29 13:11:06 +0200

    src/editor.zig: refactor: save implementation

--- src/editor.zig
+++ src/editor.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const tui = @import("./tui.zig");
+const fs = std.fs;
 
 const print = std.debug.print;
 
@@ -7,13 +8,42 @@ pub const Editor = struct {
     x: i32,
     y: i32,
     allocator: std.mem.Allocator,
-    buffer: std.ArrayList(std.ArrayList(u8)),
+    list: std.ArrayList(std.ArrayList(u8)),
 
-    pub fn load(self: *Editor, path: []const u8) void {
+    pub fn init(allocator: std.mem.Allocator) Editor {
+        return Editor {
+            .x    = 0,
+            .y    = 0,
+            .allocator   = allocator,
+            .buffer = std.ArrayList(std.ArrayList(u8)).init(allocator)
+        };
+    }
 
+    pub fn putc(self: Editor, x: u32, y: u32) void {
+        const line_size = self.list.size();
+
+        if (y > line_size) {
+                      
+        }
+
+    }
+
+    pub fn open(self: *Editor, path: []const u8) !void {
+        var cwd = fs.cwd();
+
+        var file = try cwd.openFile(path, 
+            fs.File.OpenFlags {.mode = .read_only});
+
+        const buffer = try self.allocator.alloc(u8, @as(usize, (try file.stat()).size));
+
+        _ = try file.read(buffer);
+        print("file: {s}", .{buffer});
     }
 
     pub fn save(self: *Editor, path: []const u8) !void {
-
+        self;
+        path;
     }
+
+
 };

075767da3e1325387c0ab8f0a3cd5a008465b3b1
miesho <marcin.ryzewskii@gmail.com>
2025-07-31 17:20:36 +0200

    src/editor.zig: add: implement putc/puts/print to editor buffer

--- src/editor.zig
+++ src/editor.zig
@@ -15,17 +15,50 @@ pub const Editor = struct {
             .x    = 0,
             .y    = 0,
             .allocator   = allocator,
-            .buffer = std.ArrayList(std.ArrayList(u8)).init(allocator)
+            .list = std.ArrayList(std.ArrayList(u8)).init(allocator)
         };
     }
 
-    pub fn putc(self: Editor, x: u32, y: u32) void {
-        const line_size = self.list.size();
+    fn new_line(self: *Editor, line: u32) !void {
+        const inner = std.ArrayList(u8).init(self.allocator);
+
+        if (self.list.items.len > line) {
+            try self.list.insert(line, inner);
+        } else {
+            try self.list.append(inner);
+        }
+    }
+
+    pub fn putc(self: *Editor, ch: u8, x: u32, y: u32) !void {
+        const line_size = self.list.capacity;
 
         if (y > line_size) {
-                      
+            _ = try new_line(y);
         }
 
+        var line = self.list[y];
+        line[x] = ch;
+    }
+
+    pub fn puts(self: *Editor, s: []u8, x: u32, y: u32) !void {
+        if (y >= self.list.items.len) {
+            try self.new_line(y);
+        }
+
+        var line = &self.list.items[y];
+        var offset = x;
+        for (s) |ch| {
+            if (offset >= line.items.len) {
+                try line.append(ch);
+            } else {
+                line.items[offset] = ch;
+            }
+            offset += 1;
+        }
+    }
+
+    pub fn println(self: *Editor, s: []u8, line: u32) !void {
+        try self.puts(s, 0, line);
     }
 
     pub fn open(self: *Editor, path: []const u8) !void {
@@ -34,10 +67,16 @@ pub const Editor = struct {
         var file = try cwd.openFile(path, 
             fs.File.OpenFlags {.mode = .read_only});
 
-        const buffer = try self.allocator.alloc(u8, @as(usize, (try file.stat()).size));
+        const list = try self.allocator.alloc(u8, @as(usize, (try file.stat()).size));
 
-        _ = try file.read(buffer);
-        print("file: {s}", .{buffer});
+        _ = try file.read(list);
+        var lines = std.mem.splitScalar(u8, list, '\n');
+
+        var line_number: u32 = 0;
+        while (lines.next()) |line| {
+            try self.println(@constCast(line), line_number);
+            line_number += 1;
+        }
     }
 
     pub fn save(self: *Editor, path: []const u8) !void {
